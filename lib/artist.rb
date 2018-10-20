57a08454eb219a7fdf689e7068991e6ad5eb6253
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-10-20 21:17:25 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,34 +1,16 @@
-class Artist
-
-  attr_accessor :name
-
-  @@song_count = 0
-
-  def initialize(name)
+class Customer
+  attr_accessor :name, :age
+ 
+  @@all = []
+ 
+  def initialize(name, age)
     @name = name
-    @songs = []
+    @age = age
+    @@all << self
   end
-
-  def songs
-    @songs
+ 
+  def self.all
+    @@all
   end
-
-  def add_song(song)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
-  end
-
-
-  def add_song_by_name(name)
-    song = Song.new(name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
-  end
-
-  def self.song_count
-    @@song_count
-  end
-
+ 
 end
